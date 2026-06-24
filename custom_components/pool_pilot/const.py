ce4ed2eb9dbf0c1e2c30c7b53a93244ed8511193
amery74 <168977314+amery74@users.noreply.github.com>
2026-06-24 23:15:55 +0200

    v0.5.6

--- custom_components/pool_pilot/const.py
+++ custom_components/pool_pilot/const.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Final
 
 DOMAIN: Final = "pool_pilot"
-VERSION: Final = "0.5.5"
+VERSION: Final = "0.5.6"
 
 CONF_POOL_NAME: Final = "pool_name"
 CONF_VOLUME_M3: Final = "volume_m3"
